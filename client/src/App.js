8f146c8c176c57215357d5738d87928818e65f7d
Ram Singh <ramsingh83@users.noreply.github.com>
2020-11-09 14:07:19 +0530

    Update App.js
    Added to test CI

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>"Hello server!" says the client</h1>
+        <h1>"Hello server!" says the client- Test</h1>
         <h1>"{this.state.welcome}" says the server</h1>
       </div>
     );
